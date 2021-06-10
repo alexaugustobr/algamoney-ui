215ef7aae3dae681c5cec93cb60e9a46c8c0e8d7
Alex <alex.silva@algaworks.com.br>
2021-06-10 15:08:13 -0300

    url fixa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:8080',
-  apiUrl: 'https://aocosta-algamoney-api.herokuapp.com'
+  apiUrl: 'https://ag-m-app.herokuapp.com'
 };
